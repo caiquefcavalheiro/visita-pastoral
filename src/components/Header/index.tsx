8e4cfd43d8a9b7349cea46424f8e077d62e82881
ErickDev <123skatesk8@gmail.com>
2022-10-23 20:02:41 -0400

    feat: add navigate to header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,26 +1,39 @@
 import { AntDesign } from "@expo/vector-icons";
-import { Box, Center, Heading, Image } from "native-base";
+import { useNavigation } from "@react-navigation/native";
+import { Box, Center, Heading } from "native-base";
+import { TouchableOpacity } from "react-native";
 
 interface HeaderProps {
   title: string;
+  path?: string;
 }
 
-export const Header = ({ title }: HeaderProps) => {
+export const Header = ({ title, path }: HeaderProps) => {
+  const navigate = useNavigation();
+
   return (
     <Center
       w="100%"
       minH="110px"
       bg="yellow.300"
-      flexDir="row"
-      justifyContent="space-between"
       px={7}
       shadow="9"
       paddingTop={5}
+      flexDir="row"
+      position="relative"
     >
-      <Box>
-        <AntDesign name="left" size={40} color="white" />
-      </Box>
-      <Heading size="xl" color="white" textAlign="center" mr="30%">
+      {path && (
+        <Box position="absolute" left="5%" top="50%" zIndex={0}>
+          <TouchableOpacity
+            onPress={() => {
+              navigate.navigate(path as never);
+            }}
+          >
+            <AntDesign name="left" size={40} color="white" />
+          </TouchableOpacity>
+        </Box>
+      )}
+      <Heading size="xl" color="white" textAlign="center">
         {title}
       </Heading>
     </Center>
