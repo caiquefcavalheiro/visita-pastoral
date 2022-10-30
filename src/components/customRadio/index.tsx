a460eabfeba4823f20aed04feeb9268b71baebbc
ErickDev <123skatesk8@gmail.com>
2022-10-30 10:54:23 -0400

    feat: create the CustomRadio

--- src/components/customRadio/index.tsx
+++ src/components/customRadio/index.tsx
@@ -1,4 +1,5 @@
 import {
+  FormControl,
   IRadioGroupProps,
   IRadioProps,
   ITextProps,
@@ -6,6 +7,13 @@ import {
   Stack,
   Text,
 } from "native-base";
+import {
+  Control,
+  Controller,
+  FieldError,
+  FieldErrorsImpl,
+  Merge,
+} from "react-hook-form";
 
 type customRadioProps = {
   options: {
@@ -15,6 +23,10 @@ type customRadioProps = {
   }[];
   radioGroupProps: IRadioGroupProps;
   textStyle?: ITextProps;
+  label?: string;
+  name?: string;
+  control: Control<any>;
+  error?: FieldError | Merge<FieldError, FieldErrorsImpl<any>> | undefined;
 };
 
 const defaultStyle = {
@@ -26,31 +38,68 @@ const defaultTextStyle = {
   fontSize: 16,
 };
 
-export const customRadio = ({
+const defaultLabelStyle: ITextProps = {
+  fontSize: "20",
+};
+
+export const CustomRadio = ({
   options,
   radioGroupProps,
   textStyle,
+  error,
+  label,
+  name,
+  control,
 }: customRadioProps) => {
   return (
-    <Radio.Group {...radioGroupProps}>
-      <Stack
-        direction={{
-          base: "row",
-        }}
-        alignItems={{
-          base: "flex-start",
-          md: "center",
-        }}
-        space={4}
-        minW="20%"
-        maxW="100%"
-      >
-        {options.map(({ value, label, style = {} }) => (
-          <Radio value={value} key={value} {...{ ...defaultStyle, ...style }}>
-            <Text {...{ ...defaultTextStyle, ...textStyle }}>{label}</Text>
-          </Radio>
-        ))}
-      </Stack>
-    </Radio.Group>
+    <Controller
+      control={control}
+      name={name || ""}
+      render={({ field: { onChange, value } }) => (
+        <FormControl w="100%" isInvalid={error ? true : false}>
+          {label && (
+            <FormControl.Label>
+              <Text {...defaultLabelStyle}>{label}</Text>
+            </FormControl.Label>
+          )}
+          <Radio.Group
+            {...radioGroupProps}
+            onChange={onChange}
+            defaultValue={value}
+          >
+            <Stack
+              direction={{
+                base: "row",
+              }}
+              alignItems={{
+                base: "flex-start",
+                md: "center",
+              }}
+              justifyContent="space-between"
+              px="10"
+              space={5}
+              w="100%"
+            >
+              {options.map(({ value, label, style = {} }) => (
+                <Radio
+                  value={value}
+                  key={value}
+                  {...{ ...defaultStyle, ...style }}
+                >
+                  <Text {...{ ...defaultTextStyle, ...textStyle }}>
+                    {label}
+                  </Text>
+                </Radio>
+              ))}
+            </Stack>
+          </Radio.Group>
+          {error && (
+            <FormControl.ErrorMessage>
+              {error?.message}
+            </FormControl.ErrorMessage>
+          )}
+        </FormControl>
+      )}
+    />
   );
 };
