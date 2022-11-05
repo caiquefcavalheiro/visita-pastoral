e6b5a2b70205a71a553db73506426753c48150aa
ErickDev <123skatesk8@gmail.com>
2022-11-05 10:28:03 -0400

    feat: add families to routes

--- src/views/Dashboard/views/Church/routes/index.tsx
+++ src/views/Dashboard/views/Church/routes/index.tsx
@@ -1,6 +1,7 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { memo } from "react";
 import Church from "..";
+import Families from "../views/Families";
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
@@ -8,6 +9,7 @@ function ChurchRoutes() {
   return (
     <Navigator screenOptions={{ headerShown: false }} initialRouteName="Church">
       <Screen name="Church" component={Church} />
+      <Screen name="Families" component={Families} />
     </Navigator>
   );
 }
