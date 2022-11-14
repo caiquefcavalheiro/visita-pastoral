2fc78931525e7fb8b3cb7865882ea055e8765e54
ErickDev <123skatesk8@gmail.com>
2022-11-14 09:51:18 -0400

    .

--- src/components/CameraAndGalery/index.tsx
+++ src/components/CameraAndGalery/index.tsx
@@ -3,7 +3,7 @@ import * as ImagePicker from "expo-image-picker";
 import { BackHandler } from "react-native";
 
 import { Actionsheet, HStack, Icon, Text } from "native-base";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 type CameraAndGaleryProps = {
   isOpen: boolean;
@@ -26,58 +26,47 @@ export function CameraAndGalery({
     const { granted } = await ImagePicker.requestCameraPermissionsAsync();
 
     setPickImageCameraPermission(granted);
-    // if (status === "granted" || pickImageCameraPermission) {
-    const result = await ImagePicker.launchCameraAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      allowsEditing: true,
-      aspect: [4, 3],
-      quality: 1,
-      base64: true,
-    });
+    if (granted || pickImageCameraPermission) {
+      const result = await ImagePicker.launchCameraAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+        base64: true,
+      });
 
-    if (!result.cancelled && result?.uri) {
-      handleSelectImage(result?.uri);
-      onClose();
+      if (!result.cancelled && result?.uri) {
+        handleSelectImage(result?.uri);
+        onClose();
+      }
+    } else {
+      // BackHandler.exitApp();
+      alert("Não conseguimos obter a permição");
     }
-    // } else {
-    //   // BackHandler.exitApp();
-    //   alert("Não conseguimos obter a permição");
-    // }
   };
 
   const pickImageGalery = async () => {
     const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync();
 
     setPickImageGaleryPermission(granted);
-    // if (status === "granted" || pickImageGaleryPermission) {
-    const result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      allowsEditing: true,
-      aspect: [4, 3],
-      quality: 1,
-      base64: true,
-    });
+    if (granted || pickImageGaleryPermission) {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+        base64: true,
+      });
 
-    if (!result.cancelled && result?.uri) {
-      handleSelectImage(result?.uri);
-      onClose();
+      if (!result.cancelled && result?.uri) {
+        handleSelectImage(result?.uri);
+        onClose();
+      }
+    } else {
+      // BackHandler.exitApp();
+      alert("Não conseguimos obter a permição");
     }
-    // } else {
-    //   // BackHandler.exitApp();
-    //   alert("Não conseguimos obter a permição");
-    // }
   };
-
-  // useEffect(() => {
-  //   (async () => {
-  //     const { granted } =
-  //       await ImagePicker.requestMediaLibraryPermissionsAsync();
-  //     setPickImageGaleryPermission(granted);
-  //     const { granted: grantedC } =
-  //       await ImagePicker.requestCameraPermissionsAsync();
-  //     setPickImageCameraPermission(grantedC);
-  //   })();
-  // }, []);
 
   return (
     <Actionsheet
@@ -94,23 +83,12 @@ export function CameraAndGalery({
             name="perm-media"
             onPress={pickImageGalery}
           />
-          <Text>
-            {pickImageGaleryPermission
-              ? "pickImageGaleryPermission"
-              : "not pickImageGaleryPermission"}
-          </Text>
-
           <Icon
             as={MaterialIcons}
             size="60"
             name="camera-alt"
             onPress={pickImageCamera}
           />
-          <Text>
-            {pickImageCameraPermission
-              ? "pickImageCameraPermission"
-              : "not pickImageCameraPermission"}
-          </Text>
         </HStack>
       </Actionsheet.Content>
     </Actionsheet>
